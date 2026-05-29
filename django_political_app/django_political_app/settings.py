f8916ffd1739c71b3df15c33fa0fa98e16ffd212
Alexandre Crestien <alexandre.crestien@email.com>
2026-05-29 10:17:56 +0200

    fix bug

--- django_political_app/django_political_app/settings.py
+++ django_political_app/django_political_app/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'app.docker.localhost']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://app.docker.localhost']
 # Application definition
 
 INSTALLED_APPS = [
