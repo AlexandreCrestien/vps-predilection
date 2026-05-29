2aa3fc168b54b009dd7b133f055adec1e3deeb4a
Alexandre Crestien <alexandre.crestien@email.com>
2026-05-29 15:33:59 +0200

    add conffiguration https

--- django_political_app/django_political_app/settings.py
+++ django_political_app/django_political_app/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'app.docker.localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'app.docker.localhost', 'predilection.duckdns.org']
 
-CSRF_TRUSTED_ORIGINS = ['https://app.docker.localhost']
+CSRF_TRUSTED_ORIGINS = ['https://app.docker.localhost', 'https://predilection.duckdns.org']
+
 # Application definition
 
 INSTALLED_APPS = [
